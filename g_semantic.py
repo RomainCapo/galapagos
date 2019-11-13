6af3b3cf2ad284f9d0edcee0390e3089694a6a10
jonas.freiburg <jonas.freiburghaus@he-arc.ch>
2019-11-13 11:06:14 +0100

    refactored visiti children

--- g_semantic.py
+++ g_semantic.py
@@ -2,63 +2,82 @@ import AST
 from AST import addToClass
 from g_parser import parse
 
+def visit_children(children):
+    for child in children:
+        child.semantic()
+
 @addToClass(AST.ProgramNode)
 def semantic(self):
     print("Program node")
-    for child in self.children:
-        child.semantic()
+    print(f"\n {self.children}")
+    visit_children(self.children)
 
 @addToClass(AST.TokenNode)
 def semantic(self):
     print("Token node")
+    print(f"\n {self.children}")
 
 @addToClass(AST.OpNode)
 def semantic(self):
     print("Op node")
+    print(f"\n {self.children}")
 
 @addToClass(AST.AssignNode)
 def semantic(self):
     print("Assign node")
+    print(f"\n {self.children}")
 
 @addToClass(AST.AvancerNode)
 def semantic(self):
     print("Avancer node")
+    print(f"\n {self.children}")
 
 @addToClass(AST.ReculerNode)
 def semantic(self):
     print("Reculer node")
+    print(f"\n {self.children}")
 
 @addToClass(AST.DecollerNode)
 def semantic(self):
     print("Decoller node")
+    print(f"\n {self.children}")
 
 @addToClass(AST.AtterrirNode)
 def semantic(self):
     print("Atterrir node")
+    print(f"\n {self.children}")
 
 @addToClass(AST.TournerGaucheNode)
 def semantic(self):
     print("Tourner gauche node")
+    print(f"\n {self.children}")
 
 @addToClass(AST.TournerDroiteNode)
 def semantic(self):
     print("Tourner droite node")
+    print(f"\n {self.children}")
 
 @addToClass(AST.PositionXNode)
 def semantic(self):
     print("Position x node")
+    print(f"\n {self.children}")
 
 @addToClass(AST.PositionYNode)
 def semantic(self):
     print("Position y node")
+    print(f"\n {self.children}")
 
 @addToClass(AST.TqNode)
 def semantic(self):
     print("Tq node")
+    print(f"\n {self.children}")
+    visit_children(self.children)
 
 @addToClass(AST.SiNode)
 def semantic(self):
     print("Si node")
+    print(f"\n {self.children}")
+    visit_children(self.children)
 
 if __name__ == "__main__":
     import sys, os
