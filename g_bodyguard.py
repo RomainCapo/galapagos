d59e78c2a53e7a055fc9a8db9bc36438781a1804
jonas.freiburg <jonas.freiburghaus@he-arc.ch>
2019-12-11 11:09:34 +0100

    add observable and in galapagos check

--- g_bodyguard.py
+++ g_bodyguard.py
@@ -4,20 +4,67 @@ class Bodyguard:
         self.dict_turtle = {}
         self.dict_galapagos = {}
 
-    def safety_check(self):
-        pass
+    def add_galapagos(self, galapagos_name, galapagos):
+        self.dict_galapagos[galapagos_name] = galapagos
 
-class Turtle:
+    def add_turtle(self, turtle_name, turtle):
+        self.dict_turtle[turtle_name] = turtle
+        self.dict_turtle[turtle_name].add_observer(self)
+
+    def safety_check(self, turtle):
+        print("Should check safety")
+        galapagos = self.dict_galapagos[turtle.g]
+        if self._is_out_galapagos(galapagos, turtle):
+            print("Out of galapagos")
+        else:
+            print("Is safe")
+
+    def _is_out_galapagos(self, galapagos, turtle):
+        return turtle.x < galapagos.x or turtle.x > galapagos.x + galapagos.width \
+            or turtle.y < galapagos.y or turtle.y > galapagos.y + galapagos.height
+
+class Observable:
+
+    def __init__(self):
+        self.observers = []
+
+    def add_observer(self, observer):
+        self.observers.append(observer)
+
+    def notify_observer(self):
+        for observer in self.observers:
+            observer.safety_check(self)
+
+class Turtle(Observable):
+
     def __init__(self, x, y, alpha, g):
+        super(Turtle, self).__init__()
         self.x = x
         self.y = y
         self.alpha = alpha
 
         self.g = g
 
+    def update_pos(self, pos):
+        self.x += pos
+        self.notify_observer()
+
 class Galapagos:
+
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
+
+if __name__ == "__main__":
+    b = Bodyguard()
+
+    g = Galapagos(0, 0, 100, 100)
+    b.add_galapagos('g1', g)
+
+    t = Turtle(0, 0, 30, 'g1')
+    b.add_turtle("t1", t)
+
+    t.update_pos(100)
+    t.update_pos(1000)
