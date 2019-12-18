828bacd66ee4a7520289e563120bbebab694e9c6
jonas.freiburg <jonas.freiburghaus@he-arc.ch>
2019-12-18 11:14:58 +0100

    add unit test on multiple files

--- unit_test.py
+++ unit_test.py
@@ -8,11 +8,17 @@ class TestCompilerMethods(unittest.TestCase):
     SUCCESS_RET_CODE = 0
 
     def test_lexer(self):
-        sp = subprocess.Popen(['python', 'g_compiler.py', 'inputs/input.galapagos'], stdout=subprocess.PIPE)
-        streamdata = sp.communicate()[0]
-        rc = sp.returncode
-        print("Return code : ", rc)
-        self.assertEqual(rc, self.SUCCESS_RET_CODE)
+        BASE_PATH = "inputs"
+        test_files = [os.path.join(BASE_PATH, file) for file in os.listdir() if file.endswith(".txt")]
+
+        return_codes = []
+        for file in test_files:
+            sp = subprocess.Popen(['python', 'g_compiler.py', file], stdout=subprocess.PIPE)
+            streamdata = sp.communicate()[0]
+            return_codes.append(sp.returncode)
+
+        expected_status_code = [self.SUCCESS_RET_CODE for i in range(0, len(test_files))]
+        self.assertEqual(return_codes, [self.SUCCESS_RET_CODE for i in range(0, len(test_files))])
 
 if __name__ == '__main__':
     unittest.main()
